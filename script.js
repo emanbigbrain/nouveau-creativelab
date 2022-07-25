d5bbfdbfab699ffe9b0e8de4a2932ae9eb90bd70
emanbigbrain <luem21th@student.ju.se>
2022-07-25 23:04:11 +0200

    drop down list and favicon

---
+++ script.js
@@ -0,0 +1,81 @@
+let card1 = document.getElementById("card1");
+let card2 = document.getElementById("card2");
+let card3 = document.getElementById("card3");
+let card4 = document.getElementById("card4");
+
+let click = false;
+let click2 = false;
+let click3 = false;
+let click4 = false;
+
+card1.addEventListener("click", () => {
+  if (click === false) {
+    gsap.to("#card1", {
+      height: "auto",
+      duration: 1,
+      ease: "expo",
+    });
+    click = true;
+  } else if (click === true) {
+    gsap.to("#card1", {
+      height: "15vh",
+      duration: 1,
+      ease: "expo",
+    });
+    click = false;
+  }
+});
+
+card2.addEventListener("click", () => {
+  if (click2 === false) {
+    gsap.to("#card2", {
+      height: "auto",
+      duration: 1,
+      ease: "expo",
+    });
+    click2 = true;
+  } else if (click2 === true) {
+    gsap.to("#card2", {
+      height: "15vh",
+      duration: 1,
+      ease: "expo",
+    });
+    click2 = false;
+  }
+});
+
+card3.addEventListener("click", () => {
+  if (click3 === false) {
+    gsap.to("#card3", {
+      height: "auto",
+      duration: 1,
+      ease: "expo",
+    });
+    click3 = true;
+  } else if (click3 === true) {
+    gsap.to("#card3", {
+      height: "15vh",
+      duration: 1,
+      ease: "expo",
+    });
+    click3 = false;
+  }
+});
+
+card4.addEventListener("click", () => {
+  if (click4 === false) {
+    gsap.to("#card4", {
+      height: "auto",
+      duration: 1,
+      ease: "expo",
+    });
+    click4 = true;
+  } else if (click4 === true) {
+    gsap.to("#card4", {
+      height: "15vh",
+      duration: 1,
+      ease: "expo",
+    });
+    click4 = false;
+  }
+});
